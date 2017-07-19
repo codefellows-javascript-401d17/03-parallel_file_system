c39c376ceeb5b297f6a057bb5b9504bee0145bd8
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-19 14:17:56 -0700

    Fixed bug in recurssive file reader

--- lab/lib/file-reader.js
+++ lab/lib/file-reader.js
@@ -1,27 +1,22 @@
 'use strict';
 
 const fs = require('fs');
-const errorCheck = ('../my-mods/error-check.js');
 
 const fileReader = module.exports = function(path, callback, fileIndex=0) {
-  let allFiles;
-  fs.readdir(`${__dirname}/../data`, (err, dir) => {
-    if(err) throw err;
-    allFiles = dir;
-  });
-  let fileName = allFiles[fileIndex];
 
-  fs.readFile(fileName, function(err, asset) {
+  fs.readdir(path, (err, dir) => {
     if(err) throw err;
-    callback(null, asset);
-
-    fileIndex += 1;
-    if(fileName !== allFiles[allFiles.length - 1]) fileReader(path, callback, fileIndex);
+    asyncWrapper(dir);
   });
+
+  function asyncWrapper(file) {
+    let fileName = file[fileIndex];
+    fs.readFile(`${path}/${fileName}`, function(err, asset) {
+      if(err) throw err;
+      callback(null, asset);
+
+      fileIndex += 1;
+      if(fileIndex < file.length) fileReader(path, callback, fileIndex);
+    });
+  }
 };
-
-
-fs.readdir(`${__dirname}/../data`, (err, dir) => {
-  if(err) throw err;
-  console.log(dir);
-});
