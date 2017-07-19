26782a81c51c35d03506efff60e1d796dc5eff0e
ohjonah <ohjonah@gmail.com>
2017-07-19 14:25:20 -0700

    Cleans Up Code

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 const fileReader = require('./lib/file-reader.js');
 
-fileReader(`${__dirname}/assets/one.txt`, function(err, data) {
+fileReader(`${__dirname}/assets/one.txt`, (err, data) => {
   if (err) throw err;
   console.log('content of one.txt:', data);
 
-  fileReader(`${__dirname}/assets/two.txt`, function(err, data) {
+  fileReader(`${__dirname}/assets/two.txt`, (err, data) => {
     if (err) throw err;
     console.log('content of two.txt:', data);
 
-    fileReader(`${__dirname}/assets/three.txt`, function(err, data) {
+    fileReader(`${__dirname}/assets/three.txt`, (err, data) => {
       if (err) throw err;
       console.log('content of three.txt:', data);
     });
--- test/file-reader-test.js
+++ test/file-reader-test.js
@@ -3,28 +3,31 @@
 const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
-describe('File Reader Module', function() {
-  describe('with improper file path', function() {
-    it('should return an error', function(done) {
-      fileReader(`${__dirname}/not-a-file.txt`, function(err) {
+describe('File Reader Module', () => {
+  describe('with improper file path', () => {
+    it('should return an error', done => {
+      fileReader(`${__dirname}/not-a-file.txt`, err => {
         expect(err).to.be.an('error');
         done();
       });
     });
   });
 
-  describe('with a proper file path', function() {
-    it('should return content from txt files', function(done) {
-      fileReader(`${__dirname}/../assets/one.txt`, function(err, data) {
+  describe('with a proper file path', () => {
+    it('should return content from txt files', done => {
+      fileReader(`${__dirname}/../assets/one.txt`, (err, data) => {
         expect(err).to.equal(null);
+        expect(data.length).to.equal(16);
         expect(data).to.be.a('string');
 
-        fileReader(`${__dirname}/../assets/two.txt`, function(err, data) {
+        fileReader(`${__dirname}/../assets/two.txt`, (err, data) => {
           expect(err).to.equal(null);
+          expect(data.length).to.equal(16);
           expect(data).to.be.a('string');
 
-          fileReader(`${__dirname}/../assets/two.txt`, function(err, data) {
+          fileReader(`${__dirname}/../assets/two.txt`, (err, data) => {
             expect(err).to.equal(null);
+            expect(data.length).to.equal(16);
             expect(data).to.be.a('string');
             done();
           });
