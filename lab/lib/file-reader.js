d047e7391c09dea06b94c4dab43f1308c1e7fe11
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-19 13:37:41 -0700

    Created recurssive file reader that iterates through a directory and reads contents of each file

--- lab/lib/file-reader.js
+++ lab/lib/file-reader.js
@@ -4,13 +4,24 @@ const fs = require('fs');
 const errorCheck = ('../my-mods/error-check.js');
 
 const fileReader = module.exports = function(path, callback, fileIndex=0) {
-  let fileName = '';
-  
+  let allFiles;
+  fs.readdir(`${__dirname}/../data`, (err, dir) => {
+    if(err) throw err;
+    allFiles = dir;
+  });
+  let fileName = allFiles[fileIndex];
+
   fs.readFile(fileName, function(err, asset) {
     if(err) throw err;
     callback(null, asset);
-    fileIndex += 1;
 
-    if(fileName !== 'last file') fileReader(path, callback, fileIndex);
+    fileIndex += 1;
+    if(fileName !== allFiles[allFiles.length - 1]) fileReader(path, callback, fileIndex);
   });
 };
+
+
+fs.readdir(`${__dirname}/../data`, (err, dir) => {
+  if(err) throw err;
+  console.log(dir);
+});
