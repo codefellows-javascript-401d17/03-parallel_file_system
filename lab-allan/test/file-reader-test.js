a2fe164a5e90ee8c6d1ad78fa26da948091af7bd
Allan Liebold <allan.liebold@gmail.com>
2017-07-19 14:40:01 -0700

    changes it string to describe the test more accurately

--- lab-allan/test/file-reader-test.js
+++ lab-allan/test/file-reader-test.js
@@ -13,7 +13,7 @@ describe('File Reader Module', function(){
     });
   });
   describe('with a proper file path', function() {
-    it('should return the content of the data', (done) => {
+    it('should return the first 8 bytes from assets in order', (done) => {
       fileReader(`${__dirname}/../assets/one.txt`, (err, asset) => {
         expect(err).to.equal(null);
         expect(asset).to.be.a('string');
