fdcc26f6a7754fb3d7c587748b776f50ed938054
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-19 14:55:29 -0700

    got the buffer to work

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 'use strict';
 
-const filereader = require('../03-parallel_file_system/lib/file-reader.js');
+const fs = require('fs');
+const fileReader = require('../03-parallel_file_system/lib/file-reader.js');
+//
+// var filePaths = [`${__dirname}/assets/one.txt`,`${__dirname}/assets/two.txt`, `${__dirname}/assets/three.txt`];
+//
+// var printHexInOrder = (err) => {
+//   if(err) throw err;
+//   filePaths.map((path) => {
+//     fs.fileReader();
+//   });
+// };
+//
+// printHexInOrder();
 
-var filePaths = [`${__dirname}/assets/one.txt`,`${__dirname}/assets/two.txt`, `${__dirname}/assets/three.txt`];
+fileReader(`${__dirname}/assets/one.txt`, function(err, data){
+  console.log(data);
+  fileReader(`${__dirname}/assets/two.txt`, function(err, data){
+    console.log(data);
+    fileReader(`${__dirname}/assets/three.txt`, function(err, data){
+      console.log(data);
 
-var printHexInOrder = (err) => {
-  if(err) throw err;
-  filePaths.map((path) => {
-    console.log(path);
-    console.log(fileReader(path));
+    });
   });
-};
-
-printHexInOrder();
+});
