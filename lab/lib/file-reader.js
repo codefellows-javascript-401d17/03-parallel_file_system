192737533fac39f4c0c124e3727d73d533bfefa2
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-19 16:04:14 -0700

    Got it to pass the cursed test

--- lab/lib/file-reader.js
+++ lab/lib/file-reader.js
@@ -4,26 +4,24 @@ const fs = require('fs');
 
 const fileReader = module.exports = function(path, callback) {
 
-  function asyncWrapper(folder, callback, fileIndex=0) {
-    let fileName = folder[fileIndex];
-    fs.readFile(`${path}/${fileName}`, function(err, asset) {
+  function asyncWrapper(folder, callback, buffArray) {
+    let fileName = folder.pop();
+    return fs.readFile(`${path}/${fileName}`, function(err, asset) {
 
       if(err) return callback(err);
-      asset = Array.prototype.slice.call(asset, 0, 8);
-      console.log(asset);
-      callback(null, asset);
-
-      fileIndex += 1;
-      if(fileIndex < folder.length) return asyncWrapper(folder, callback, fileIndex);
-      return;
+      buffArray.push(asset.toString('hex', 0, 8));
+      console.log(buffArray[buffArray.length -1]);
+      if(folder.length !== 0) return asyncWrapper(folder, callback, buffArray);
+      return callback(err, buffArray);
     });
   }
 
   return fs.readdir(path, (err, dir) => {
     if(err) return callback(err);
+    let buffArray = [];
     //This is a hack to get them to read in order....
     //May the coding gods have mercy on my soul...
-    dir.sort((a, b) => a.length - b.length);
-    asyncWrapper(dir, callback);
+    dir.sort((a, b) => a.length - b.length).reverse();
+    asyncWrapper(dir, callback, buffArray);
   });
 };
--- lab/test/file-reader-test.js
+++ lab/test/file-reader-test.js
@@ -17,7 +17,11 @@ describe('File Reader Module', () => {
   describe('With proper file path', () => {
     it('Should throw console log the first 8 items in the buffer', (done) => {
       fileReader(`${__dirname}/../data`, (err, asset) => {
-      })
+        expect(asset[0]).to.equal('5468697320697320');
+        expect(asset[1]).to.equal('49207265616c6c79');
+        expect(asset[2]).to.equal('4261636f6e206772');
+        done();
+      });
     });
   });
 });
