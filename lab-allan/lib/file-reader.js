4eefeda150c3963319b46ba515c08967b3296f2d
Allan Liebold <allan.liebold@gmail.com>
2017-07-19 14:35:54 -0700

    refactored with lexical arrow functions

--- lab-allan/lib/file-reader.js
+++ lab-allan/lib/file-reader.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 const fileReader = module.exports = function(file, callback) {
-  fs.readFile(file, function(err, asset) {
+  fs.readFile(file, (err, asset) => {
     if(err) return callback(err);
     return callback(null, asset.toString('hex', 0, 8));
   });
--- lab-allan/test/file-reader-test.js
+++ lab-allan/test/file-reader-test.js
@@ -4,25 +4,25 @@ const expect = require('chai').expect;
 const fileReader = require('../lib/file-reader.js');
 
 describe('File Reader Module', function(){
-  describe('with an improper file path', function() {
-    it('should return an error', function(done) {
-      fileReader(`${__dirname}/not-a-file.txt`, function(err) {
+  describe('with an improper file path', () => {
+    it('should return an error', (done) => {
+      fileReader(`${__dirname}/not-a-file.txt`, (err) => {
         expect(err).to.be.an('error');
         done();
       });
     });
   });
   describe('with a proper file path', function() {
-    it('should return the content of the data', function(done) {
-      fileReader(`${__dirname}/../assets/one.txt`, function(err, asset) {
+    it('should return the content of the data', (done) => {
+      fileReader(`${__dirname}/../assets/one.txt`, (err, asset) => {
         expect(err).to.equal(null);
         expect(asset).to.be.a('string');
         expect(asset.length).to.equal(16);
-        fileReader(`${__dirname}/../assets/two.txt`, function(err, asset) {
+        fileReader(`${__dirname}/../assets/two.txt`, (err, asset) => {
           expect(err).to.equal(null);
           expect(asset).to.be.a('string');
           expect(asset.length).to.equal(16);
-          fileReader(`${__dirname}/../assets/three.txt`, function(err, asset) {
+          fileReader(`${__dirname}/../assets/three.txt`, (err, asset) => {
             expect(err).to.equal(null);
             expect(asset).to.be.a('string');
             expect(asset.length).to.equal(16);
