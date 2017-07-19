e0b33ddbed1222d6ade44d49ddb66701e926fbc7
andyfiveeleven <fouracesguitar@gmail.com>
2017-07-19 15:54:35 -0700

    finished testing paths, watch out for the ..'s in your file paths!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const paths = [
   `${__dirname}/assets/one.txt`,
   `${__dirname}/assets/two.txt`,
   `${__dirname}/assets/three.txt`
-]
+];
 
 readFiles(paths, function(err, data){
   console.log('Some data:', data);
