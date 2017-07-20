fafc69269402e22cd2f3192ac9952f477133871e
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-19 17:26:27 -0700

    finally finished all testing, need to work on my linting

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 'use strict';
 
-const fs = require('fs');
 const fileReader = require('../03-parallel_file_system/lib/file-reader.js');
-//
-// var filePaths = [`${__dirname}/assets/one.txt`,`${__dirname}/assets/two.txt`, `${__dirname}/assets/three.txt`];
-//
-// var printHexInOrder = (err) => {
-//   if(err) throw err;
-//   filePaths.map((path) => {
-//     fs.fileReader();
-//   });
-// };
-//
-// printHexInOrder();
 
-fileReader(`${__dirname}/assets/one.txt`, function(err, data){
-  console.log(data);
-  fileReader(`${__dirname}/assets/two.txt`, function(err, data){
+
+let orderHexBytes = module.exports = (callback) => {
+  let hexArray = [];
+  fileReader(`${__dirname}/assets/one.txt`, function(err, data){
     console.log(data);
-    fileReader(`${__dirname}/assets/three.txt`, function(err, data){
+    hexArray.push(data);
+    fileReader(`${__dirname}/assets/two.txt`, function(err, data){
       console.log(data);
-
+      hexArray.push(data);
+      fileReader(`${__dirname}/assets/three.txt`, function(err, data){
+        console.log(data);
+        hexArray.push(data);
+        callback(hexArray);
+      });
     });
   });
-});
+};
