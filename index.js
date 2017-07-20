dfcfe6917c22599f35ac52006715d50b1643a1f2
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-07-19 17:45:26 -0700

    finished assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fileReader = require('../03-parallel_file_system/lib/file-reader.js');
 
 
-let orderHexBytes = module.exports = (cb) => {
+let orderHexBytes = module.exports = (cb) => { //eslint-disable-line
   let hexArray = [];
   fileReader(`${__dirname}/assets/one.txt`, function(err, data){
     if(err) return cb(err);
